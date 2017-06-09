2d8d5098f308700ff5029de56acc795596b62bc5
webFarrock <webFarrock@mail.ru>
2017-06-09 13:51:50 +0300

    Section 3, Lecture 15

--- 02-app/src/server/server.js
+++ 02-app/src/server/server.js
@@ -1,6 +1,56 @@
 import 'source-map-support/register';
-import {blegh} from 'shared/test';
 
-import _ from 'lodash';
+import express from 'express';
+import http from 'http';
+import socketIo from 'socket.io';
 
-blegh();
+const IsDevelopment = process.env.NODE_ENV !== 'production';
+
+// -------------------------
+// Setup
+
+const app = express();
+const server =  new http.Server(app);
+const io = socketIo(server);
+
+
+
+// -------------------------
+// Client Webpack
+
+
+// -------------------------
+// Configure Express
+app.set('view engine', 'jade');
+app.use(express.static('public'));
+
+const useEcternalStyles = !IsDevelopment;
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        useEcternalStyles
+    });
+})
+
+// -------------------------
+// Modules
+
+
+// -------------------------
+// Socket
+io.on('connection', socket => {
+    console.log(`Got connection from ${socket.request.connection.remoteAdress}`);
+});
+
+
+// -------------------------
+// Startup
+
+const port = process.env.PORT || 3000;
+
+(function startServer(){
+    server.listen(port, () => {
+        console.log(`Started http server on ${port}`);
+    })
+})();
+
